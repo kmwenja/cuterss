b25c86cbbf7c3d5f3371a833a2196ee53df4cbc0
caninemwenja <mwenja07@gmail.com>
2013-07-25 00:51:00 +0300

    can read and print out title and desc of rss feed

--- index.php
+++ index.php
@@ -6,3 +6,14 @@ $feed = new SimplePie();
 
 $feed->set_feed_url('./rss.xml');
 
+$feed->init();
+
+$feed->handle_content_type();
+
+echo $feed->get_title();
+
+echo "<br/>";
+
+echo $feed->get_description();
+
+echo "<br/>";
